80041a7322acc499fa113f028e640f698829c997
Siddhant Mishra <siddhant48311@gmail.com>
2021-02-04 05:29:38 +0530

    Intersection part completed

--- Arrays/src/UnionAndIntersection.java
+++ Arrays/src/UnionAndIntersection.java
@@ -1,7 +1,7 @@
 
 public class UnionAndIntersection {
 	
-	public static void printUnion(int arr1[], int arr2[], int m, int n) { // function that prints union of arr1 and arr2
+	public static void printUnion(int arr1[], int arr2[], int m, int n) { // function that prints union of arr1 and arr2. O(m+n)
 		int i = 0,j = 0;
 		while(i < m && j < n) {
 			if(arr1[i] < arr2[j])
@@ -18,13 +18,29 @@ public class UnionAndIntersection {
 		while(j<n)
 			System.out.print(arr2[j++]);
 	}
+	
+	public static void printIntersection(int arr1[], int arr2[], int m, int n) {
+		int i = 0, j = 0;
+		while(i < m && j < n) {
+			if(arr1[i] < arr2[j])
+				i++;
+			else if (arr2[j] < arr1[i])
+				j++;
+			else {
+				System.out.print(arr1[i++]);
+				j++;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		int arr1[] = {1,2,4,5,8};
-		int arr2[] = {1,3,4,5};
+		int arr1[] = {1, 2, 4, 5};
+		int arr2[] = {1, 3, 4, 5, 8};
 		int m = arr1.length;
 		int n = arr2.length;
 		printUnion(arr1, arr2, m, n);
+		System.out.println("");
+		printIntersection(arr1, arr2, m, n);
 	}
 
 }
